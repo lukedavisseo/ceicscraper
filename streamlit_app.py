c5253618a73b23d16c70b454cffa6f231e1d2c20
lukedavisseo <92871067+lukedavisseo@users.noreply.github.com>
2021-10-20 16:10:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,15 +65,11 @@ if submit:
 			link = (org[results]['link'])
 			for c in competitor_urls:
 				if c in link:
-					serp['urls'].append(link)
-					serp['titles'].append(org[results]['title'])
-					serp['meta_desc'].append(org[results]['snippet'])
 					serp['competitor'].append("Competitor match found")
 					st.write(f'Competitor match found: {c}')
-				else:
-					serp['urls'].append(link)
-					serp['titles'].append(org[results]['title'])
-					serp['meta_desc'].append(org[results]['snippet'])
+			serp['urls'].append(link)
+			serp['titles'].append(org[results]['title'])
+			serp['meta_desc'].append(org[results]['snippet'])
 					
 		df = {key:pd.Series(value, dtype='object') for key, value in serp.items()}
 		serp_df = pd.DataFrame(df)
