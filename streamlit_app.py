775ac092e4577949d0253f1258a1134e55382fa5
lukedavisseo <92871067+lukedavisseo@users.noreply.github.com>
2021-10-22 13:32:48 +0100

    Added retry code

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ from scraper_api import ScraperAPIClient
 import pandas as pd
 import json
 import tldextract
+import time
 
 api_key = st.text_input("Enter ScraperAPI key")
 
@@ -53,25 +54,29 @@ if submit:
 		payload = {'api_key': api_key, 'url': google_search_url, 'autoparse': 'true'}
 		headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 		proxy_url = requests.get('http://api.scraperapi.com/?', payload, headers=headers)
-
+		
 		try:
-			response = proxy_url.json()
-			org = response.get("organic_results")
+			for i in range(5):
+				time.sleep(2)
+				response = proxy_url.json()
+				org = response.get("organic_results")
 
-			for results in range(0,len(org)):
-				link = (org[results]['link'])
-				ext = tldextract.extract(link)
-				if ext.registered_domain in competitor_urls:
-					serp['urls'].append(link)
-					serp['titles'].append(org[results]['title'])
-					serp['meta_desc'].append(org[results]['snippet'])
-					serp['competitor'].append("Competitor match found")
-					st.write(f'Competitor match found: {ext.registered_domain}')
-			if not serp['competitor']:
-				st.write('No competitors found')
+				for results in range(0,len(org)):
+					link = (org[results]['link'])
+					ext = tldextract.extract(link)
+					if ext.registered_domain in competitor_urls:
+						serp['urls'].append(link)
+						serp['titles'].append(org[results]['title'])
+						serp['meta_desc'].append(org[results]['snippet'])
+						serp['competitor'].append("Competitor match found")
+						st.write(f'Competitor match found: {ext.registered_domain}')
+				if not serp['competitor']:
+					st.write('No competitors found')
+				break
 					
 		except (ValueError, Timeout, SSLError, MissingSchema) as e:
 			st.error(f"{e} found! Continuing...")
+			continue
 			
 		df = {key:pd.Series(value, dtype='object') for key, value in serp.items()}
 		serp_df = pd.DataFrame(df)
